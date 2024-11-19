2343f0e5dbe021419cd67931f49717cf3e6b44ef
Pratik9307 <121624387+Pratik9307@users.noreply.github.com>
2024-11-19 15:06:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "*",
-    origin: "sanjivani-website.netlify.app",
+    origin: "http://sanjivani-website.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
